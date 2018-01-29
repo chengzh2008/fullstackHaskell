1a74bb572d98fc27f52d6a1e4374a4227e5fcb4d
Zhihong Cheng <chengzh2008@gmail.com>
2018-01-28 20:26:18 -0800

    update

--- frontend/src/Main.hs
+++ frontend/src/Main.hs
@@ -5,5 +5,12 @@ import Language.Javascript.JSaddle.Warp
 import Reflex.Dom.Core (mainWidget)
 import Reflex.Dom hiding (mainWidget, run)
 
+app = mainWidget $ el "div" $ do
+  el "p" $ text "Reflex is:"
+  el "ul" $ do
+    el "li" $ text "Efficient1"
+    el "li" $ text "Higher-order"
+    el "li" $ text "Glitch-free"
+
 main :: IO ()
-main = run 3911 $ mainWidget $ el "div" $ text "Welcome to Reflex"
+main = run 3911 $ app
